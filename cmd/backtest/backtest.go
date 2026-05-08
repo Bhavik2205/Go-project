e028ac86ceaa46967618bd13c96ef47af53e50ac
Bhavik Patel <bhavik252patel@gmail.com>
2026-05-08 09:56:58 +0530

    fix backtest by adding main function skeleton

--- cmd/backtest/backtest.go
+++ cmd/backtest/backtest.go
@@ -1,1 +1,6 @@
 package main
+
+func main() {
+	// This is a placeholder for the backtesting logic.
+	// You can implement your backtesting code here, using the same ZerodhaAPI interface and database client as in the server.
+}
